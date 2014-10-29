e7a82fe766681aa68ddde41f64b2639e7105eeac
yousuketto <yousuketto>
2014-10-29 22:37:16 +0900

    able to create dependency graph using d3.js

--- app/controllers/nanahoshi/info_controller.rb
+++ app/controllers/nanahoshi/info_controller.rb
@@ -1,4 +1,24 @@
+require 'bundler'
 class Nanahoshi::InfoController < ApplicationController
   def index
+    lookup = Bundler.load.specs.to_hash
+    hash = lookup.each_with_object({}) do |(gemname, specs), hash|
+      hash[gemname] = specs.map do |spec|
+        spec.dependencies.select{|dependency|dependency.type == :runtime}.map(&:name)
+      end.flatten.uniq
+    end
+
+    gem_names = hash.keys
+    @nodes = gem_names.map{|gem_name|{name: gem_name}}
+
+    @edges = []
+    hash.each do |gem, deps|
+      source_index = gem_names.index(gem)
+      dependence_edges = deps.map do |dep|
+        {source: source_index, target: gem_names.index(dep)}
+      end
+
+      @edges.concat(dependence_edges)
+    end
   end
 end
